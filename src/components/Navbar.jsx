712937f68a45b6d693f177a50cf61dba624888b9
Lisa Smeke <lisasmeke@gmail.com>
2023-08-29 18:49:28 +0200

    Add Home & Navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      {/* <h1>The wedding site</h1> */}
+      <div className="links">
+        <a href="/">N&S</a>
+        <a href="/boda">Boda</a>
+        <a href="/hospedaje">Hospedaje</a>
+        <a href="/mesaregalos">Mesa regalos</a>
+        <a href="/rsvp">RSVP</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
