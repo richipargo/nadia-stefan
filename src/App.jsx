f6956bf3d4940d9c4e77f26537b09cb036df3eee
Lisa Smeke <lisasmeke@gmail.com>
2023-09-01 18:24:44 +0200

    Move Hero to Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+
 import Home from './components/Home';
 import Home1 from './components/Home1';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './index.css';
 
 function App() {
   // const title = 'Welcome to our party';
 
   return (
-    <>
-      <div>
+    <Router>
+      <div className="app">
         <Navbar />
-        <Hero />
-        <Home />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+        {/* <Hero /> */}
         {/* <div>
           <Home1 />
         </div> */}
         <Footer />
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import '../style_components/home.css';
+import Hero from './Hero';
 import Countdown from './Countdown';
 
 const Home = () => {
   return (
     <div className="home">
+      <Hero />
       <Countdown />
       <p className="when-where">
         27 Enero, 2024. 2:00<span className="pm-span"> pm</span> | Jardín Xolatlaco
