ccba52178b3d0ed528b42d845171260b26ab4c6b
Lisa Smeke <lisasmeke@gmail.com>
2023-09-01 23:02:38 +0200

    Customize style for initials Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import '../style_components/footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <p className="initials">N&S</p>
+      <Link className="link-initials" to="/">
+        <p className="initials">N&S</p>
+      </Link>
       <p className="date">27.01.2024</p>
       <footer>
         <p className="copyright">&copy; 2023 by LSM</p>
