8a1cfd3d5ae2da30405022017090617723a37723
Lisa Smeke <lisasmeke@gmail.com>
2023-09-20 19:42:02 +0200

    Add info to Boda & add image for instructions

--- src/components/Boda.jsx
+++ src/components/Boda.jsx
@@ -4,59 +4,98 @@ import Map from './Map';
 const Boda = () => {
   return (
     <div className="boda-container">
-      <div>
+      <div className="boda-title">
         <h1>
           Estamos muy felices y agradecidos de que nos acompañen a celebrar el día de nuestra boda.
         </h1>
       </div>
-      <div className="cuando-donde">
-        <div className="location-text">
+      {/*Cuando y Dónde*/}
+      <div className="cuando-donde-container">
+        {/*Cuando y Dónde initial info*/}
+        <div className="cuando-donde-text">
           <h2>Cuándo y Dónde</h2>
           <p>Sábado 27 de enero, 2024</p>
-          <p>13.30 hrs.</p>
+          <p>13:30 hrs.</p>
           <p>Jardín Xolatlaco</p>
           <p>
             Carretera Tepoztlán - Amatlán no. 1000 - B Amatlán de Quetzalcóatl, Tepoztlán Morelos CP
             62525
           </p>
-          <p>
-            *Buscar en Waze/Google Maps “Jardin Xolatlaco” (NO solamente “Xolatlaco”, ya que te
-            puede llevar a otro lugar).
-          </p>
-          <p>
-            Seguir
-            <a className="address-link" href="https://waze.com/ul/h9g3ecwhep" target="_blank">
-              Waze
-            </a>
-          </p>
-
-          <p>
-            Seguir{' '}
-            <a
-              className="address-link"
-              href="https://maps.app.goo.gl/ENe7YmdFubLPBpYC8"
-              target="_blank"
-            >
-              Google Maps
-            </a>
-          </p>
         </div>
-        <div className="location-map">
+        {/*Cuando y Dónde map*/}
+        <div className="cuando-donde-map">
           <Map />
         </div>
+        {/*Cómo llegar*/}
+        <div className="como-llegar">
+          <div className="como-llegar-text">
+            <h3>Cómo llegar</h3>
+            <p>
+              Sigue{' '}
+              <a
+                className="address-link"
+                href="https://waze.com/ul/h9g3ecwhep"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Waze
+              </a>{' '}
+              ó{' '}
+              <a
+                className="address-link"
+                href="https://maps.app.goo.gl/ENe7YmdFubLPBpYC8"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Google Maps
+              </a>
+            </p>
+            <p>
+              El destino debe ser “Jardín Xolatlaco” (NO solamente “Xolatlaco”, ya que te puede
+              llevar a otro lugar).
+            </p>
+            <p>
+              Por favor tomen en cuenta que los sábados suele haber mucho tráfico en el centro de
+              Tepoztlán, les pedimos considerarlo dentro de sus planes.
+            </p>
+            <p>
+              Habrá servicio de valet parking para quienes lleven coche, pero recomendamos llegar en
+              taxi/Uber si es que piensan tomar durante el fiestón. A la salida, habrá servicio de
+              camionetas hacia los hoteles en los siguientes horarios: X, X, y X.{' '}
+            </p>
+            <p>
+              Al llegar, Jardín Xolatlaco se encontrará a mano derecha, verán una puerta grande de
+              madera y un letrero que dice “XOLATLACO”
+            </p>
+          </div>
+          <div className="como-llegar-images">
+            <img
+              src="./public/xolatlaco1.png"
+              alt="Puerta grande de madera en la entrada de Jardín Xolatlaco"
+            />
+            {/* <img src="./public/xolatlaco2.png" alt="" /> */}
+          </div>
+        </div>
       </div>
-
-      <div className="dresscode">
+      {/*Ceremonia/Recepción*/}
+      <div className="event-container">
+        <h2>Ceremonia / Recepción</h2>
+        <p>Todo el evento será en Jardín Xolatlaco.</p>
+        <p>Los esperamos a las 13:30hrs para la ceremonia.</p>
+        <p>De ahí pasaremos al cóctel y recepción.</p>
+      </div>
+      {/*Dresscode*/}
+      <div className="dresscode-container">
         <h2>Dresscode</h2>
         <p>Formal</p>
-        <p>Recuerda traer algo abrigador para la noche.</p>
+        <p>Mujeres - vestido midi o largo (de día) </p>
+        <p>Hombres - traje</p>
+        <p>
+          Durante el día esperamos que el clima esté bonito/caluroso, pero en la noche hará frío,
+          ¡por favor lleven algo abrigador!{' '}
+        </p>
       </div>
 
-      <div className="rsvp">
-        <h2>¿Podrás acompañarnos?</h2>
-        <p>¡Te esperamos con toda la emoción y alegría del mundo!</p>
-        <p>Por favor confirma tu asistencia dando click en el botón de RSVP.</p>
-      </div>
       <img className="hero-img" src="./ns-243.jpg" alt="Nadia y Stefan en Oaxaca" />
     </div>
   );
--- src/components/Rsvp.jsx
+++ src/components/Rsvp.jsx
@@ -1,7 +1,9 @@
 const Rsvp = () => {
   return (
     <div className="rsvp">
-      <h2>Rsvp aquí</h2>
+      <h2>¿Podrás acompañarnos?</h2>
+      <p>¡Te esperamos con toda la emoción y alegría del mundo!</p>
+      <p>Por favor confirma tu asistencia dando click en el botón de RSVP.</p>
     </div>
   );
 };
