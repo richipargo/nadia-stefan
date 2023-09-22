6f80c26af30ae5b58eb63dfb3cab75f09d03c90c
Lisa Smeke <lisasmeke@gmail.com>
2023-09-22 14:53:23 +0200

    Update boda title & ceremonia & recepción

--- src/components/Boda.jsx
+++ src/components/Boda.jsx
@@ -5,14 +5,25 @@ const Boda = () => {
   return (
     <div className="boda-container">
       <div className="boda-title">
-        <h1>
-          Estamos muy felices y agradecidos de que nos acompañen a celebrar el día de nuestra boda.
-        </h1>
+        <div className="boda-title-bouquet-left">
+          <img className="boda-bouquet-left" src="./bouquet3-min-crop.png" alt="Flower bouquet" />
+        </div>
+        <div className="boda-title-text">
+          <h1>
+            ¡Hola! Estamos muy felices y agradecidos de que nos acompañes a celebrar el día de
+            nuestra boda. Aquí encontrarás todos los detalles el evento.
+          </h1>
+          <p className="boda-title-p">¡Esperamos verte en enero para celebrar en grande!</p>
+          <p className="signature">- Nadia & Stefan</p>
+        </div>
+        <div className="boda-title-bouquet-right">
+          <img className="boda-bouquet-right" src="./bouquet3-min-crop.png" alt="Flower bouquet" />
+        </div>
       </div>
       {/*Cuando y Dónde*/}
-      <div className="cuando-donde-container">
-        {/*Cuando y Dónde initial info*/}
-        <div className="cuando-donde-text">
+      {/* <div className="cuando-donde-container"> */}
+      {/*Cuando y Dónde initial info*/}
+      {/* <div className="cuando-donde-text">
           <h2>Cuándo y Dónde</h2>
           <p>Sábado 27 de enero, 2024</p>
           <p>13:30 hrs.</p>
@@ -21,13 +32,13 @@ const Boda = () => {
             Carretera Tepoztlán - Amatlán no. 1000 - B Amatlán de Quetzalcóatl, Tepoztlán Morelos CP
             62525
           </p>
-        </div>
-        {/*Cuando y Dónde map*/}
-        <div className="cuando-donde-map">
+        </div> */}
+      {/*Cuando y Dónde map*/}
+      {/* <div className="cuando-donde-map">
           <Map />
-        </div>
-        {/*Cómo llegar*/}
-        <div className="como-llegar">
+        </div> */}
+      {/*Cómo llegar*/}
+      {/* <div className="como-llegar">
           <div className="como-llegar-text">
             <h3>Cómo llegar</h3>
             <p>
@@ -73,16 +84,51 @@ const Boda = () => {
               src="./public/xolatlaco1.png"
               alt="Puerta grande de madera en la entrada de Jardín Xolatlaco"
             />
-            {/* <img src="./public/xolatlaco2.png" alt="" /> */}
           </div>
         </div>
-      </div>
+      </div> */}
       {/*Ceremonia/Recepción*/}
-      <div className="event-container">
-        <h2>Ceremonia / Recepción</h2>
-        <p>Todo el evento será en Jardín Xolatlaco.</p>
-        <p>Los esperamos a las 13:30hrs para la ceremonia.</p>
-        <p>De ahí pasaremos al cóctel y recepción.</p>
+      <div className="when-where-container">
+        <div className="when">
+          <h2>Ceremonia & Recepción</h2>
+          <h3>Cuándo</h3>
+          <p>Sábado 27 de enero, 2024</p>
+          <p>Te esperamos a las 13:30hrs para la ceremonia.</p>
+          <p>De ahí pasaremos al cóctel y recepción.</p>
+        </div>
+        <div className="where">
+          <h3>Dónde</h3>
+          <p>Jardín Xolatlaco</p>
+          <p>
+            Carretera Tepoztlán - Amatlán no. 1000 - B Amatlán de Quetzalcóatl, Tepoztlán Morelos CP
+            62525
+          </p>
+          <button>Sigue Waze</button>
+          <button>Sigue Google Maps</button>
+          <p>
+            El destino debe ser “Jardín Xolatlaco” (NO solamente “Xolatlaco”, ya que te puede llevar
+            a otro lugar).
+          </p>
+          <p>
+            Por favor toma en cuenta que los sábados suele haber mucho tráfico en el centro de
+            Tepoztlán, te pedimos considerarlo dentro de tus planes.
+          </p>
+          <p>
+            Habrá servicio de valet parking para quienes lleven coche, pero te recomendamos llegar
+            en taxi/Uber si vas a tomar durante el fiestón. A la salida habrá servicio de camionetas
+            hacia los hoteles en los siguientes horarios: X, X, y X.{' '}
+          </p>
+          <p>
+            Al llegar, Jardín Xolatlaco se encontrará a mano derecha, verás una puerta grande de
+            madera y un letrero que dice “XOLATLACO”
+          </p>
+        </div>
+        <div className="where-img">
+          <img
+            src="./public/xolatlaco1.png"
+            alt="Puerta grande de madera en la entrada de Jardín Xolatlaco."
+          />
+        </div>
       </div>
       {/*Dresscode*/}
       <div className="dresscode-container">
