449f2d6780ebbdc7bbcdbcd84958fee4b0f4eb7a
Lisa Smeke <lisasmeke@gmail.com>
2023-09-23 21:58:07 +0200

    Style text in Boda component

--- src/components/Boda.jsx
+++ src/components/Boda.jsx
@@ -20,39 +20,49 @@ const Boda = () => {
         <div className="ceremonia-recepcion">
           <div className="ceremonia-recepcion-text">
             <h2>Ceremonia & Recepción</h2>
-            <p>Sábado 27 de enero, 2024 en Jardín Xolatlaco</p>
-            <p>Te esperamos a las 13:30hrs para la ceremonia.</p>
-            <p>De ahí pasaremos al cóctel y recepción.</p>
-            <h3>Cómo llegar</h3>
-            <p>
+            <p className="main-info">Sábado 27 de enero, 2024 a las 13:30hrs</p>
+            <p className="main-info">Jardín Xolatlaco</p>
+            <p className="note">
               Carretera Tepoztlán - Amatlán no. 1000 - B Amatlán de Quetzalcóatl, Tepoztlán Morelos
               CP 62525
             </p>
-            <button>Sigue Waze</button>
-            <button>Sigue Google Maps</button>
-            <p>
-              El destino debe ser “Jardín Xolatlaco” (NO solamente “Xolatlaco”, ya que te puede
-              llevar a otro lugar).
-            </p>
-            <p>
-              Por favor toma en cuenta que los sábados suele haber mucho tráfico en el centro de
-              Tepoztlán, te pedimos considerarlo dentro de tus planes.
-            </p>
-            <p>
-              Habrá servicio de valet parking para quienes lleven coche, pero te recomendamos llegar
-              en taxi/Uber si vas a tomar durante el fiestón. A la salida habrá servicio de
-              camionetas hacia los hoteles en los siguientes horarios: X, X, y X.{' '}
-            </p>
-            <p>
-              Al llegar, Jardín Xolatlaco se encontrará a mano derecha, verás una puerta grande de
-              madera y un letrero que dice “XOLATLACO”
-            </p>
-            <br></br>
-            <img
+            <ul className="boda-ul">
+              <li>Te esperamos a las 13:30hrs para la ceremonia.</li>
+              <li>De ahí pasaremos al cóctel y recepción.</li>
+            </ul>
+            <div className="como-llegar">
+              <h3>Cómo llegar</h3>
+              <a href="https://waze.com/ul/h9g3ecwhep" target="_blank" rel="noreferrer">
+                <button className="map-btn">Sigue Waze</button>
+              </a>
+              <a href="https://maps.app.goo.gl/ENe7YmdFubLPBpYC8" target="_blank" rel="noreferrer">
+                <button className="map-btn">Sigue Google Maps</button>
+              </a>
+              <p className="note">
+                El destino debe ser “Jardín Xolatlaco” (NO solamente “Xolatlaco”, ya que te puede
+                llevar a otro lugar).
+              </p>
+              <ul className="boda-ul">
+                <li>
+                  Por favor toma en cuenta que los sábados suele haber mucho tráfico en el centro de
+                  Tepoztlán, te pedimos considerarlo dentro de tus planes.
+                </li>
+                <li>
+                  Al llegar, Jardín Xolatlaco se encontrará a mano derecha; verás una puerta grande
+                  de madera y un letrero que dice “XOLATLACO”.
+                </li>
+                <li>
+                  Habrá servicio de valet parking, pero te recomendamos llegar en taxi/Uber si vas a
+                  tomar durante el fiestón. A la salida habrá servicio de camionetas hacia los
+                  hoteles en los siguientes horarios: X, X, y X.{' '}
+                </li>
+              </ul>
+            </div>
+            {/* <img
               className="entrance-img"
               src="./public/xolatlaco1.png"
               alt="Puerta grande de madera en la entrada de Jardín Xolatlaco."
-            />
+            /> */}
           </div>
           <div className="ceremonia-recepcion-img-container">
             <img
@@ -65,17 +75,24 @@ const Boda = () => {
       </div>
       {/*Dresscode*/}
       <div className="dresscode-container">
-        <h2>Dresscode</h2>
-        <p>Formal</p>
-        <p>Mujeres - vestido midi o largo (de día) </p>
-        <p>Hombres - traje</p>
-        <p>
-          Durante el día esperamos que el clima esté bonito/caluroso, pero en la noche hará frío,
-          ¡por favor lleven algo abrigador!{' '}
-        </p>
+        <div className="dresscode">
+          <div className="dresscode-img-container">
+            <img className="dresscode-img" src="./MER_5764.jpg" alt="Nadia y Stefan en Oaxaca" />
+          </div>
+          <div className="dresscode-text">
+            <h2>Dresscode</h2>
+            <p className="main-info">Formal</p>
+            <ul className="boda-ul">
+              <li>Mujeres: vestido midi o largo (de día) </li>
+              <li>Hombres: traje</li>
+            </ul>
+            <p className="note">
+              Durante el día esperamos que el clima esté bonito/caluroso, pero en la noche hará
+              frío, ¡por favor lleva algo abrigador!{' '}
+            </p>
+          </div>
+        </div>
       </div>
-
-      <img className="hero-img" src="./ns-243.jpg" alt="Nadia y Stefan en Oaxaca" />
     </div>
   );
 };
